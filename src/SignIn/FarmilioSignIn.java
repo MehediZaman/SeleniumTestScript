7b3e7b6fc0507d7fb2f8b3136604cc5f3376bfe4
MehediZaman <mehedi@nascenia.com>
2015-02-04 12:42:14 +0600

    'WorkOnRecommendFarmilio'

--- src/SignIn/FarmilioSignIn.java
+++ src/SignIn/FarmilioSignIn.java
@@ -2,9 +2,6 @@ package SignIn;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-
-
-import java.util.List;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -23,7 +20,7 @@ public class FarmilioSignIn {
 	      String appUrl = "http://staging.farmilio.net/";
 	    
 	 // Clear cache and cookies    
-	    //driver.manage().deleteAllCookies(); //delete all cookies
+	    driver.manage().deleteAllCookies(); //delete all cookies
 	  
 	// launch the firefox browser and open the application url
 	    driver.get(appUrl);
@@ -34,17 +31,19 @@ public class FarmilioSignIn {
 	//click on the Login button
 	    WebElement logInNav = driver.findElement(By.partialLinkText("Log"));
 	    logInNav.click();
-	   
+ 
 	 //create new account (Deutsch interface)
 	    WebElement createAccount = driver.findElement(By.partialLinkText("Konto"));
 	    createAccount.click();    
-	   
+
+	  // Wait for the Create Account page to load
 	  driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-	    
+	   
 	// enter a valid username in the email textbox
 	      WebElement usernamereg = driver.findElement(By.name("user[email]"));
 	      usernamereg.clear();
-	      usernamereg.sendKeys("mehedizaman@rocketmail.com");
+	      //usernamereg.sendKeys("mehedizaman@rocketmail.com");
+	      usernamereg.sendKeys("mehedizaman@ymail.com");
 	    
 	// enter a valid password in the password textbox
 	    WebElement passwordreg = driver.findElement(By.name("user[password]"));
@@ -55,19 +54,66 @@ public class FarmilioSignIn {
 	   // WebElement checkboxreg = driver.findElement(By.id("user_terms_and_conditions"));
 	 	//checkboxreg.click();
 	  	    
+	  if ( !driver.findElement(By.id("user_terms_and_conditions")).isSelected())
+	    {
+	         driver.findElement(By.id("user_terms_and_conditions")).click();
+	    }
+	  
+	  WebElement kontoAnlegenButton = (new WebDriverWait(driver, 15))
+			   .until(ExpectedConditions.presenceOfElementLocated(By.xpath("(//button[@type='submit'])[2]")));
+			 
+			   //click on the Konto anlegen (create account) button
+			   	kontoAnlegenButton = driver.findElement(By.xpath("(//button[@type='submit'])[2]"));
+			    kontoAnlegenButton.click();
+			   
+			    
+			  
+			    
+	   // Wait for the Create Account page to load
+	   driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+	
+	   //click on the Login button
+	   //WebElement logInNav = driver.findElement(By.partialLinkText("Log"));
+	    // logInNav.click();
+	   
+	   //create new account (Deutsch interface)
+	   //WebElement createAccount = driver.findElement(By.partialLinkText("Konto"));
+	   // createAccount.click();
+	 
+	 // enter a valid username in the email textbox
+	      //WebElement usernamereg = driver.findElement(By.name("user[email]"));
+	      //usernamereg.clear();
+	      //usernamereg.sendKeys("mehedizaman@rocketmail.com");
+	      //usernamereg.sendKeys("sqamehedi@gmail.com");
+	    
+	// enter a valid password in the password textbox
+	    //WebElement passwordreg = driver.findElement(By.name("user[password]"));
+	    //passwordreg.clear();
+	    //passwordreg.sendKeys("carbon");
+	
+	 	/*  	    
 	    if ( !driver.findElement(By.id("user_terms_and_conditions")).isSelected())
 	    {
 	         driver.findElement(By.id("user_terms_and_conditions")).click();
 	    }
+	    */
 	    
-	 	
-	   WebElement kontoAnlegenButton = (new WebDriverWait(driver, 15))
-	   .until(ExpectedConditions.presenceOfElementLocated(By.xpath("(//button[@type='submit'])[2]")));
+	  // WebElement kontoAnlegenButton = (new WebDriverWait(driver, 15))
+	  // .until(ExpectedConditions.presenceOfElementLocated(By.xpath("(//button[@type='submit'])[2]")));
 	 
 	   //click on the Konto anlegen (create account) button
-	   	kontoAnlegenButton = driver.findElement(By.xpath("(//button[@type='submit'])[2]"));
-	    kontoAnlegenButton.click();
-	     	    
+	   	//kontoAnlegenButton = driver.findElement(By.xpath("(//button[@type='submit'])[2]"));
+	    //kontoAnlegenButton.click();
+	     	
+	  //click on the Login button
+	   // WebElement logInNav = driver.findElement(By.partialLinkText("Log"));
+	   // logInNav.click();
+	   
+	 //create new account (Deutsch interface)
+	   // WebElement createAccount = driver.findElement(By.partialLinkText("Konto"));
+	    //createAccount.click();   
+	    
+	   
 	    /*
 	// declare and initialize the variable to store the expected title of the webpage.
 	    String expectedTitle = " Farmilio ";
@@ -87,6 +133,7 @@ public class FarmilioSignIn {
 	           System.out.println("Verification Failed - An incorrect title is displayed on the web page.");
 	    }*/
 
+	    
 	  //click on the Login button
 	    WebElement logInInitial = driver.findElement(By.partialLinkText("Log"));
 	    logInInitial.click();
@@ -105,9 +152,9 @@ public class FarmilioSignIn {
 	    WebElement logInButton = driver.findElement(By.xpath("//button[@type='submit']"));
 	    logInButton.click();
 	    
-	    String homeUrl = "http://staging.farmilio.net/";
-	    driver.get(homeUrl); 
-	      	    
+	   // String homeUrl = "http://staging.farmilio.net/";
+	    //driver.get(homeUrl); 
+	    	    
 	 //Enter offer name/s in the Searchbox
 	    WebElement searchBox = driver.findElement(By.xpath("//input[@type='text']"));
 	    searchBox.clear();
@@ -176,7 +223,41 @@ public class FarmilioSignIn {
 	//click on the Speichern / Submit button in the Benutzer Details (Edit User details) page
 			    WebElement searchAdvancedButton = driver.findElement(By.xpath("(//button[@type='submit'])[2]"));
 			    searchAdvancedButton.click();   
-			   
+
+	//click on the Share (Empfehlen) Link
+			      WebElement shareFarmilioLink = driver.findElement(By.partialLinkText("Teilen"));
+			      shareFarmilioLink.click();	
+			      
+	//click on the Recommend Farmilio(Empfehlen) Link
+			      WebElement recommendFarmilioLink = driver.findElement(By.partialLinkText("Empfehlen"));
+			      recommendFarmilioLink.click();			
+
+    //click on the Template (Vorlage) Link		      
+			      WebElement templateButton = driver.findElement(By.partialLinkText("Vorlage"));
+			      templateButton.click();
+
+	//Enter email address in to the receiver field (Vorlage) Link		      
+			      WebElement inputEmailReceiver = driver.findElement(By.id("inputEmail3"));
+			      inputEmailReceiver.clear();
+			      inputEmailReceiver.sendKeys("mehedizaman@rocketmail.com,mehedizmn@yahoo.com");
+			      
+	//Enter the subject in the email of the Recommend Farmilio form	      
+			      WebElement inputEmailSubject = driver.findElement(By.name("subject"));
+			      inputEmailSubject.clear();
+			      inputEmailSubject.sendKeys("Farmilio MVP 1.0 Selenium Test Automation");
+			      
+	//Enter the email message in the email of the Recommend Farmilio form		      
+			      WebElement inputEmailMessage = driver.findElement(By.name("message"));
+			      inputEmailMessage.clear();
+			      inputEmailMessage.sendKeys("Ich habe eine neue Internet-Plattform entdeckt die Dir sicher auch gefallen wird. Auf FARMILIO findest Du aktuelle Angebote von Bauern, Jägern und Fischern in Deiner Nachbarschaft.Direkt vom Bauern - beste Qualität und ein Beitrag zum Erhalt der heimischen, bäuerlichen Landwirtschaft.");
+			      
+	//Submit the Recommend Farmilio form	      
+			      //WebElement inputEmailSubmit = driver.findElement(By.xpath("//button[@type='submit'])[2]"));
+			      //WebElement inputEmailSubmit = driver.findElement(By.partialLinkText("Abschicken"));
+			      //WebElement inputEmailSubmit = driver.findElement(By.xpath("//button[@type='submit'])"));
+			      WebElement inputEmailSubmit = driver.findElement(By.cssSelector("button.send-email.btn.btn-default"));
+			      inputEmailSubmit.click();
+			      
 	// close the web browser
 	    //driver.close();
 	    System.out.println("Test script executed successfully.");
